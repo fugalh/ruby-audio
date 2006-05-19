a396af6fbffc758bab123b121614e62d3d396130
hans <hans@fugal.net>
2006-05-19 10:06:15 -0600

    extconf.rb
    darcs-hash:20060519160615-28bda-90da11c43061cfda98680f0ab2fec9810c23bd44.gz

--- ext/sndfile/extconf.rb
+++ ext/sndfile/extconf.rb
@@ -1,4 +1,12 @@
 require 'mkmf'
 system 'swig -ruby sndfile.i'
-$libs = append_library $libs, 'sndfile'
+unless find_library 'sndfile', 'sf_open'
+  error 'You need to install libsndfile'
+  exit
+end
+dir_config 'narray', CONFIG['sitearchdir'], CONFIG['sitearchdir']
+unless have_header 'narray.h'
+  error 'You need to install NArray'
+  exit
+end
 create_makefile 'sndfile'
--- ext/sndfile/narray_config.h
+++
@@ -1,5 +0,0 @@
-#define HAVE_SYS_TYPES_H 1
-#define HAVE_U_INT8_T 1
-#define HAVE_INT16_T 1
-#define HAVE_INT32_T 1
-#define HAVE_U_INT32_T 1
