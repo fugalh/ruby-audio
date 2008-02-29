627e2d2789cac50078a031bc6ed5d463e4799bee
hans <hans@fugal.net>
2008-02-28 21:35:20 -0700

    narray gem todo
    darcs-hash:20080229043520-28bda-77bb4b9704fb871effef8921f664a72c3431db67.gz

--- ext/sndfile/extconf.rb
+++ ext/sndfile/extconf.rb
@@ -7,6 +7,7 @@ unless find_library 'sndfile', 'sf_open'
   exit
 end
 dir_config 'narray', CONFIG['sitearchdir'], CONFIG['sitearchdir']
+# TODO /opt/local/lib/ruby/gems/1.8/gems/narray-0.5.9.4/narray.h
 unless have_header 'narray.h'
   error 'You need to install NArray'
   exit
